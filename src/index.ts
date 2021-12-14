cbdbb8f36e6c69be7e03a0836c6a29cf8a67875d
Kenji Saito <1867845+poad@users.noreply.github.com>
2021-12-14 09:11:23 +0900

    Fix/esm defined (#476)
    * Fix the es module defined
    
    * Fix ES Modules

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,29 @@
 import jwt from 'jsonwebtoken';
 
-export module JwtGenerator {
-  const signOption = (
-    issuerId: string,
-    privateKeyId: string,
-    duration: number,
-  ): jwt.SignOptions => ({
-    algorithm: 'ES256',
-    keyid: privateKeyId,
-    audience: 'appstoreconnect-v1',
-    expiresIn: duration,
-    issuer: issuerId,
-  });
-  export const tokenSync = (
-    privateKey: string | Buffer,
-    issuerId: string,
-    privateKeyId: string,
-    duration: number = 500,
-  ): string => jwt.sign({}, privateKey, signOption(issuerId, privateKeyId, duration));
+const signOption = (
+  issuerId: string,
+  privateKeyId: string,
+  duration: number,
+): jwt.SignOptions => ({
+  algorithm: 'ES256',
+  keyid: privateKeyId,
+  audience: 'appstoreconnect-v1',
+  expiresIn: duration,
+  issuer: issuerId,
+});
 
-  export const token = async (
-    privateKey: string | Buffer,
-    issuerId: string,
-    privateKeyId: string,
-    duration: number = 500,
-  ): Promise<string> => Promise.resolve(
-    jwt.sign({}, privateKey, signOption(issuerId, privateKeyId, duration)),
-  );
-}
+export function tokenSync(
+  privateKey: string | Buffer,
+  issuerId: string,
+  privateKeyId: string,
+  duration: number = 500,
+): string { return jwt.sign({}, privateKey, signOption(issuerId, privateKeyId, duration)) };
 
-export default JwtGenerator;
+export async function token(
+  privateKey: string | Buffer,
+  issuerId: string,
+  privateKeyId: string,
+  duration: number = 500,
+): Promise<string> { return Promise.resolve(
+  jwt.sign({}, privateKey, signOption(issuerId, privateKeyId, duration))
+)};
