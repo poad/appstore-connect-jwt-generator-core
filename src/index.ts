d3e9f1c49c4985c40194acaeeb63d5e21c34a251
Kenji Saito <1867845+poad@users.noreply.github.com>
2021-12-16 21:35:42 +0900

    Fix the type declaration (#499)

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+export declare function tokenSync(
+    privateKey: string | Buffer,
+    issuerId: string,
+    privateKeyId: string,
+    duration: number | undefined,
+): string;
+
+export declare function token(
+    privateKey: string | Buffer,
+    issuerId: string,
+    privateKeyId: string,
+    duration: number | undefined,
+): Promise<string>;
