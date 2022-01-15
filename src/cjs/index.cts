8a2cae98b65653db2b13d5fe7b6e4206efd75bf6
Kenji Saito <1867845+poad@users.noreply.github.com>
2022-01-16 04:36:19 +0900

    Fix the module not found (#586)

--- src/cjs/index.cts
+++ src/cjs/index.cts
@@ -12,18 +12,29 @@ const signOption = (
   issuer: issuerId,
 });
 
+function jwtGenCore() { }
+
 export function tokenSync(
   privateKey: string | Buffer,
   issuerId: string,
   privateKeyId: string,
   duration: number = 500,
-): string { return jwt.sign({}, privateKey, signOption(issuerId, privateKeyId, duration)) };
+): string { return jwt.sign({}, privateKey, signOption(issuerId, privateKeyId, duration)); };
 
 export async function token(
   privateKey: string | Buffer,
   issuerId: string,
   privateKeyId: string,
   duration: number = 500,
-): Promise<string> { return Promise.resolve(
-  jwt.sign({}, privateKey, signOption(issuerId, privateKeyId, duration))
-)};
+): Promise<string> {
+  return Promise.resolve(
+    jwt.sign({}, privateKey, signOption(issuerId, privateKeyId, duration))
+  );
+};
+
+jwtGenCore.tokenSync = tokenSync;
+jwtGenCore.token = token;
+
+module.exports = exports = jwtGenCore;
+
+export default jwtGenCore;
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,24 @@
+"use strict";
+
+// ref: https://github.com/tc39/proposal-global
+const getGlobal = (): any => {
+    // the only reliable means to get the global object is
+    // `Function('return this')()`
+    // However, this causes CSP violations in Chrome apps.
+    if (typeof self !== 'undefined') { return self; }
+    if (typeof window !== 'undefined') { return window; }
+    if (typeof global !== 'undefined') { return global; }
+    throw new Error('unable to locate global object');
+}
+
+const global = getGlobal();
+module.exports = exports = global.jwtGenCore;
+
+// Needed for TypeScript and Webpack.
+if (global.fetch) {
+    exports.default = global.jwtGenCore.bind(global);
+}
+
 export declare function tokenSync(
     privateKey: string | Buffer,
     issuerId: string,
@@ -11,3 +32,6 @@ export declare function token(
     privateKeyId: string,
     duration: number | undefined,
 ): Promise<string>;
+
+exports.tokenSync = tokenSync;
+exports.token = token;
