433de27769f5ddef191ebaadd13bb7fe2369af58
Kenji Saito <1867845+poad@users.noreply.github.com>
2022-05-08 21:54:21 +0900

    Use SWC for compile and test (#971)

--- package/lib/tests/commonjs/src/main.cjs
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const appstore_connect_jwt_generator_core_1 = require("appstore-connect-jwt-generator-core");
-console.log(typeof appstore_connect_jwt_generator_core_1.tokenSync);
-//# sourceMappingURL=main.cjs.map
