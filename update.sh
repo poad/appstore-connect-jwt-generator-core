e0c85cab99c3ab9d91bf6e9ff431fa029d04e6f6
Kenji Saito <ken-yo@mbr.nifty.com>
2024-04-16 23:45:40 +0900

    Bumps node modules

--- update.sh
+++ update.sh
@@ -2,21 +2,21 @@
 
 CUR=$(pwd)
 
-CURRENT=$(cd $(dirname $0);pwd)
+CURRENT=$(cd "$(dirname "$0")" || exit;pwd)
 echo "${CURRENT}"
 
-cd "${CURRENT}"
+cd "${CURRENT}" || exit
 git pull --prune
 result=$?
 if [ $result -ne 0 ]; then
-  cd "${CUR}"
+  cd "${CUR}" || exit
   exit $result
 fi
 
-cd "${CURRENT}"/package
+cd "${CURRENT}"/package || exit
 result=$?
 if [ $result -ne 0 ]; then
-  cd "${CUR}"
+  cd "${CUR}" || exit
   exit $result
 fi
 echo ""
@@ -24,14 +24,14 @@ pwd
 pnpm install && pnpm up -r && pnpm build && pnpm package
 result=$?
 if [ $result -ne 0 ]; then
-  cd "${CUR}"
+  cd "${CUR}" || exit
   exit $result
 fi
 
-cd "${CURRENT}"/tests/commonjs
+cd "${CURRENT}"/tests/commonjs || exit
 result=$?
 if [ $result -ne 0 ]; then
-  cd "${CUR}"
+  cd "${CUR}" || exit
   exit $result
 fi
 echo ""
@@ -39,14 +39,14 @@ pwd
 pnpm install && pnpm up -r && pnpm build && pnpm package
 result=$?
 if [ $result -ne 0 ]; then
-  cd "${CUR}"
+  cd "${CUR}" || exit
   exit $result
 fi
 
-cd "${CURRENT}"/tests/esm
+cd "${CURRENT}"/tests/esm || exit
 result=$?
 if [ $result -ne 0 ]; then
-  cd "${CUR}"
+  cd "${CUR}" || exit
   exit $result
 fi
 echo ""
@@ -54,21 +54,21 @@ pwd
 pnpm install && pnpm up -r && pnpm build && pnpm package
 result=$?
 if [ $result -ne 0 ]; then
-  cd "${CUR}"
+  cd "${CUR}" || exit
   exit $result
 fi
 
-cd "${CURRENT}"
+cd "${CURRENT}" || exit
 result=$?
 if [ $result -ne 0 ]; then
-  cd "${CUR}"
+  cd "${CUR}" || exit
   exit $result
 fi
 git commit -am "Bumps node modules" && git push
 result=$?
 if [ $result -ne 0 ]; then
-  cd "${CUR}"
+  cd "${CUR}" || exit
   exit $result
 fi
 
-cd "${CUR}"
+cd "${CUR}" || exit
